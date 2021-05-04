5ac851c7947812ccfb7577edf93bf7f95174efc9
MUHAMMAD ZIAD ALFIAN <muhammadziadalfian07@gmail.com>
2021-05-04 14:21:39 +0800

    Final

--- src/Container/Splash/Splash.jsx
+++ src/Container/Splash/Splash.jsx
@@ -1,10 +1,42 @@
+import { Box, Container, Link, makeStyles } from '@material-ui/core';
 import React from 'react'
-// import Image from '../../images/ic_spalsh.jpg'
+import Image from '../../images/ic_spalsh.jpg'
+import ImageBg from '../../images/spalsh.jpg'
 
+const useStyles = makeStyles((theme) => ({
+    paper: {
+        marginTop: theme.spacing(8),
+        display: 'flex',
+        flexDirection: 'column',
+        alignItems: 'center',
+        backgroundImage: `url(${ImageBg})`,
+    },
+    media: {
+        height: '165px',
+        marginBottom: '33px'
+    },
+}))
 
 const Splash = () => {
+    const classes = useStyles();
+
     return (
-        <p> Some text to fill the Paper Component</p>
+        <Container component="main" maxWidth="xs">
+            <div className={classes.paper}>
+                <Box marginTop="150px" />
+                <img src={Image} alt="" className={classes.media} />
+                <Link
+                    href="/onboarding"
+                    style={{
+                        color: "#0065B3",
+                        textDecoration: 'none',
+                        fontWeight: 700
+                    }}
+                    onClick={console.log("im buton")} >
+                    Patra Park Food Culinary
+                </Link>
+            </div>
+        </Container >
     );
 }
 
